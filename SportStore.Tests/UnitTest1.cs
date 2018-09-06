21604a56f48b41b2d52898a60e0c0f167a69f652
Ponomarenko Max <ponomarenko.max2015@gmail.com>
2018-09-06 23:14:50 +0300

    first commit

---
+++ SportStore.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SportStore.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
